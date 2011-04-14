1cb56fd0823218f4eef0a727682feb1b4ee6092e
murphy360 <murphy360@40affcd1-c18a-4a85-8587-951b829def46>
2011-04-14 15:32:01 +0000

    refactored newprofile.xml to newprofileactivity.xml IAW Andrios Naming Conventions.
    Fixed bug in Manifest where prior Refactors didn't rename activities in manifest.

--- BodyCards/src/com/andrios/bodycards/NewProfileActivity.java
+++ BodyCards/src/com/andrios/bodycards/NewProfileActivity.java
@@ -42,7 +42,7 @@ public class NewProfileActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.newprofile);
+		setContentView(R.layout.newprofileactivity);
 
 		setConnections();
 		getExtra();
