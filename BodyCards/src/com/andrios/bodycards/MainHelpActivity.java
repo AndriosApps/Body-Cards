dbb9a73bc1aa2c63db60f627599eec2bfa6c2833
murphy360 <murphy360@40affcd1-c18a-4a85-8587-951b829def46>
2011-04-29 22:57:54 +0000

    Created MainHelpActivity.java and mainhelpactivity.xml  this will replace current help activity and become portal to all other help activities.

---
+++ BodyCards/src/com/andrios/bodycards/MainHelpActivity.java
@@ -0,0 +1,89 @@
+package com.andrios.bodycards;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainHelpActivity extends Activity {
+
+	
+	Button helpGettingStartedBTN, helpDeckOfCardsBTN, helpRandomWorkoutBTN, helpCustomWorkoutBTN;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.mainhelpactivity);
+		setConnections();
+	}//onCreate()
+	
+	
+	private void setConnections() {
+		helpGettingStartedBTN = (Button) findViewById(R.id.mainHelpActivityGettingStartedBTN);
+		helpDeckOfCardsBTN= (Button) findViewById(R.id.mainHelpActivityDeckOfCardsBTN);
+		helpRandomWorkoutBTN = (Button) findViewById(R.id.mainHelpActivityRandomWorkoutBTN);
+		helpCustomWorkoutBTN = (Button) findViewById(R.id.mainHelpActivityCustomWorkoutBTN);
+		
+		setOnClickListeners();
+		
+	}
+	private void setOnClickListeners() {
+	
+		
+		helpGettingStartedBTN.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), HelpGettingStartedActivity.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+		helpDeckOfCardsBTN.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), HelpDeckOfCardsWorkoutActivity.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+		helpRandomWorkoutBTN.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), HelpRandomWorkoutActivity.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+		helpCustomWorkoutBTN.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), HelpCustomWorkoutActivity.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
