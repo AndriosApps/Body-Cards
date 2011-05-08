febf862760a74e5d95510e4975374a63d4ddf34c
murphy360 <murphy360@40affcd1-c18a-4a85-8587-951b829def46>
2011-05-08 17:19:14 +0000

    Exercises verified and cleaned up.  Pics are missing but will be added in future release.

--- BodyCards/src/com/andrios/bodycards/ExerciseListActivity.java
+++ BodyCards/src/com/andrios/bodycards/ExerciseListActivity.java
@@ -160,51 +160,127 @@ public class ExerciseListActivity extends Activity {
 		exerciseList
 				.add(0,
 						new Exercise(
-								"Push-Ups with Rotation",
-								"1. Start by lying on your back with your knees towards your chest and your arms flat on the ground.\n\n" +
-								"2. Keeping your back flat throughout the movement kick your legs out and away until they are almost straight.\n\n" +
-								"3. Bring your legs back in and repeat for the required number of repetitions.\n\n" +
-								"4. If you are unable to keep your back flat on the floor throughout the movement shorten the distance that your legs extend until you get stronger.",
-								"Chest",
-								R.drawable.pushupwithrotation));
+								"Push-Ups",
+								"1. Begin with your hands and toes on the floor.\n\n" +
+								"2. Your torso and legs should remain rigid, keeping your back perfectly straight throughout the move.\n\n" +
+								"3. Bend your arms and slowly lower your body downward, stopping just before your upper chest touches the ground.\n\n" +
+								"4. Feel a stretch in your chest muscles and then reverse direction, pushing your body up along the same path back to the start position.",
+								R.drawable.nopic,
+								1.0, 
+								"Chest", 
+								false));
 		exerciseList
 				.add(0,
 						new Exercise(
-								"Double Leg Pressouts",
-								"1. Start by lying on your back with your knees towards your chest and your arms flat on the ground.\n\n" +
-								"2. Keeping your back flat throughout the movement kick your legs out and away until they are almost straight.\n\n" +
-								"3. Bring your legs back in and repeat for the required number of repetitions.\n\n" +
-								"4. If you are unable to keep your back flat on the floor throughout the movement shorten the distance that your legs extend until you get stronger.",
-								"Abs",
-								R.drawable.doublelegpressouts));
+								"Squats",
+								"1. Stand erect with the feet about shoulder-width apart, feet pointed straight ahead or slightly toed-out.\n\n" +
+								"2. Keeping the head up, the chest lifted, and the abdominal muscles tight, squat until thighs are parallel to the floor.\n\n" +
+								"3. Arch your lower back slightly, and keep that arch throughout the exercise.\n\n" +
+								"4. Pause briefly, then stand upright by pressing heels into the floor and keeping the glutes (the rump muscles) tight.",
+								R.drawable.nopic,
+								1.0,
+								"Hamstrings",
+								false));
 		exerciseList
 				.add(0,
 						new Exercise(
-								"Hip Thrust",
-								"1. Lie on your back with your legs bent 90 degrees at the hip.\n\n" +
-								"2. Slowly lift your hips off the floor and towards the ceiling.\n\n" +
-								"3. Lower your hips to the floor and repeat for the prescribed number of repetitions.",
-								"Abs",
-								R.drawable.hipthrust));
+								"Bench Dips",
+								"1. Sit on the edge of a solid bench or chair with your hands holding the edge by your hips.\n\n" +
+								"2. Walk your feet forward, then straighten your arms to lift your body off the bench.\n\n" +
+								"3. Lower yourself until your upper arms are about parallel with the floor, then press with the triceps to lift yourself back up - straighten the arms, but don’t lock the elbows at the top."+
+								"Alternative. Place your feet on another bench or chair and add weights to your lap to add difficulty.",
+								R.drawable.nopic,
+								1.0,
+								"Triceps",
+								false));
 		exerciseList
 				.add(0,
 						new Exercise(
-								"Oblique Crunch",
-								"1. Start by placing your left foot over your right knee and place your hands at the side of your head.\n\n" +
-								"2. Lift your shoulders up off the ground and twist so that your right elbows tries to touch your left knee.\n\n" +
-								"3. Return to the starting position and repeat according to the required repetitions.\n\n" +
-								"4. Repeat with the other side.",
-								"Abs",
-								R.drawable.obliquecrunch));
+								"Calf Raises",
+								"1. Stand on the balls of your feet on the edge of a step, with your heels hanging over the edge.\n\n" +
+								"2. Hold onto the wall or a railing lightly with one hand.\n\n" +
+								"3. Keeping your head up and your torso upright, stand up on your toes, then lower yourself until your heels are slightly below the step.\n\n" +
+								"Alternative. Balance without using your hands, conduct exercise one leg at a time.",
+								R.drawable.nopic,
+								1.0,
+								"Calves",
+								false));
 		exerciseList
 				.add(0,
 						new Exercise(
-								"V-Up",
-								"1. Start position: Lie back onto floor or bench with knees bent and hands extended towards ceiling. Head should be in a neutral position with a space between chin and chest.\n\n" +
-								"2. Leading with the chin and chest towards the ceiling, contract the abdominal and raise shoulders off floor or bench. Also raise legs up towards ceiling and attempt to touch your hands to your feet.\n\n" +
-								"3. Return to start position.\n\n",
-								"Abs",
-								R.drawable.vup));
+								"Reverse Flyes",
+								"1. Using light dumbbells, sit on bench or ball and lean over holding weights in front of legs.\n\n" +
+								"2. Keep your torso parallel to the floor.\n\n" +
+								"3. Raise arms out to the side to shoulder height, keeping elbows in a fixed (slightly bent) position.\n\n" +
+								"Alternative. Conduct exercise while standing.",
+								R.drawable.nopic,
+								1.0,
+								"Back",
+								false));
+		
+		exerciseList
+				.add(0,
+						new Exercise(
+								"Jumping Jacks",
+								"1. Begin by standing with your feet together and arms at your sides. Tighten your abdominal muscles to pull your pelvis forward and take the curve out of your lower back.\n\n" +
+								"2. Bend your knees and jump, moving your feet apart until they are wider than your shoulders. At the same time, raise your arms over your head. You should be on the balls of your feet.\n\n" +
+								"3. Keep your knees bent while you jump again, bringing your feet together and your arms back to your sides. At the end of the movement, your weight should be on your heels.\n\n",
+								R.drawable.nopic,
+								2.0,
+								"Calves",
+								false));
+		
+		exerciseList
+				.add(0,
+						new Exercise(
+								"8 Count Body-Builders",
+								"Start in a standing position, arms comfortably at your sides. Keep knees slightly flexed with feet hip width apart.\n\n" +
+								"1. Squat down while keeping your back straight. Lean forward and place your hands, palms down, on the floor. Keep your arms extended so that your upper body is off the floor.\n\n" +
+								"2. Extend your legs straight out from your body, resting on your toes.\n\n" +
+								"3. Lower your upper body to the floor by bending your elbows out to the side, perpendicular to your body.\n\n" +
+								"4. Raise your upper body again, and hold it up on extended arms.\n\n" +
+								"5. Scissor your legs open.\n\n" +
+								"6. Scissor your legs closed.\n\n" +
+								"7. Draw your knees up to your chest.\n\n" +
+								"8. Quickly stand up.\n\n" +
+								"Repeat.\n\n",
+								R.drawable.nopic,
+								1.0,
+								"Hamstrings",
+								false));
+		
+		exerciseList
+			.add(0,
+					new Exercise(
+							"Sprints",
+							"Warm up with a short walk and a brief run or jog, followed by light stretching of the major muscle groups.\n\n" +
+							"1. Find a flat stretch of ground at least 100 meters long.\n\n" +
+							"2. Mark a spot in the distance, either mentally or visually, and begin running as fast as you can toward that spot.\n\n" +
+							"3. Raise your knees as high as you can with each step (think of your legs as two pistons pumping vigorously).\n\n" +
+							"4. Pump your arms hard, but keep them loose; swing them at your sides, not across your body.\n\n" +
+							"5. Breathe naturally and deeply into your abdomen as you sprint, and keep your torso erect.\n\n" +
+							"6. Slow down after you've passed your goal post, then stop.\n\n" +
+							"Repeat.\n\n",
+							R.drawable.nopic,
+							0.5,
+							"Calves",
+							false));
+		
+		exerciseList
+				.add(0,
+						new Exercise(
+								"Lunges",
+								"1. Stand in a split stance with the right foot forward and the left leg back, feet should be about 2 to 3 feet apart, depending on your leg length.\n\n" +
+								"2. Before you lunge, make sure your torso is straight and that you’re up on the back toe.\n\n" +
+								"3. Bend the knees and lower the body down until the back knee is a few inches from the floor.\n\n" +
+								"4. At the bottom of the movement, the front thigh should be parallel to the floor and the back knee should point toward the floor.\n\n" +
+								"5. Keep the weight evenly distributed between both legs and push back up, keeping the weight in the heel of the front foot.\n\n"+
+								"Repeat.\n\n",
+								R.drawable.nopic,
+								1.0,
+								"Calves",
+								false));
+		
 		exerciseList
 				.add(0,
 						new Exercise(
@@ -213,8 +289,22 @@ public class ExerciseListActivity extends Activity {
 								"2. Leading with the chin and chest towards the ceiling, contract the abdominal and raise shoulders off floor or bench until you are seated in an upright position.\n\n" +
 								"3. Return to start position.\n\n" +
 								"4. Remember to keep head and back in a neutral position. Hyperextension or flexion of either may cause injury.",
+								R.drawable.situp,
+								1.0,
 								"Abs",
-								R.drawable.situp));
+								false));
+		exerciseList
+			.add(0,
+					new Exercise(
+							"Planks",
+							"1. Start by lying face down on a mat. Place your forearms on the mat with your shoulders aligned directly over your elbows. Clasp your hands in front of you.\n\n" +
+							"2. Extend your legs behind you and rest on your toes, as if you are going to do a pushup. Your hips should not be lifted to the ceiling, nor should your back be arched. You should look to attain a straight line between your shoulders and toes.\n\n" +
+							"3. Tighten your abdominal muscles to help you hold the position correctly, and hold it as long as you can or until time expires.\n\n" +
+							"4. Alternative. Lift one leg at a time behind you to make the exercise more challenging.",
+							R.drawable.situp,
+							1.0,
+							"Abs",
+							true));
 	}
 	
 
